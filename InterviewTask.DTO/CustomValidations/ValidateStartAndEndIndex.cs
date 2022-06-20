7a3cedf4707afcdad3e62f6980578d82db667731
devmusty <mmustafayevm@gmail.com>
2022-06-20 12:43:07 +0400

    start and end index custom validation attribute fixing

--- InterviewTask.DTO/CustomValidations/ValidateStartAndEndIndex.cs
+++ InterviewTask.DTO/CustomValidations/ValidateStartAndEndIndex.cs
@@ -8,12 +8,12 @@ namespace InterviewTask.Core.CustomFilters
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             FibonacciGeneratorDTO fibonacciGeneratorDTO = (FibonacciGeneratorDTO)validationContext.ObjectInstance;
-            if (fibonacciGeneratorDTO.StartIndex < Convert.ToInt32(value))
+            if (fibonacciGeneratorDTO.StartIndex >= Convert.ToInt32(value))
             {
                 return new ValidationResult("invalid end index");
             }
 
-            return base.IsValid(value, validationContext);
+            return ValidationResult.Success;
         }
     }
 }
